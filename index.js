9f5903956078a7d3a6af559049fa1ed01230d36d
Usman Hassan <55620906+Usman-Hassan-Gu@users.noreply.github.com>
2022-03-24 08:43:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function Compare() {
+  let good = document.getElementById("good").value;
+  let bad = document.getElementById("bad").value;
+  console.log(JSON.stringify(good));
+
+  //   console.log(good === bad);
+}
