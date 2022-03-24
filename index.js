2fbf3fc4d5d068331f0aa1914c9b68fad8313213
Usman Hassan <55620906+Usman-Hassan-Gu@users.noreply.github.com>
2022-03-24 09:11:31 -0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function Compare() {
-  let good = document.getElementById("good").value;
-  let bad = document.getElementById("bad").value;
-  console.log(JSON.stringify(good));
-
-  //   console.log(good === bad);
-}
